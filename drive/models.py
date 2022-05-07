28667a2a8190114a416cdd2d17e3c4c03c50cda9
juanse1432 <jolarteu@unbosque.edu.co>
2022-05-07 00:34:41 -0500

    segundo

--- drive/models.py
+++ drive/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class drivetest(models.Model):
+    name = models.CharField(max_length=100)
+    uploadedFile = models.FileField(upload_to = "Files/")
+    date=models.DateTimeField()
+    date_created = models.DateTimeField(auto_now = True)
+    extra = models.FileField(upload_to ="Extra/")
