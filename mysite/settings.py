4b1689686a7fef10390d7ad655d699d316e1bf58
juanse1432 <jolarteu@unbosque.edu.co>
2022-06-09 00:29:20 -0500

    postsql

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':     'drivedb',
+        'USER':     'postgres',
+        'PASSWORD': 'Drivetest_1432',
+        'HOST':     'localhost',
+        'PORT':     '5432',
     }
 }
 
