44999f2e74872f3b1e0be2d92482879009153233
juanse1432 <jolarteu@unbosque.edu.co>
2022-06-11 03:54:44 -0500

    media

--- drive/urls.py
+++ drive/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 from drive.views import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import logout_then_login
+from django.views.static import serve
+from django.urls import re_path
 app_name = "drive"
 
 
@@ -13,5 +15,7 @@ urlpatterns = [
     path('agregar', login_required(DriveFormView.as_view()), name='agregar'),
     path('accounts/login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', login_required(logout_then_login), name='logout'),
-    path('editar/<int:pk>',login_required(FacturaUpdateView.as_view()), name='editar')
+    path('editar/<int:pk>',login_required(FacturaUpdateView.as_view()), name='editar'),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
